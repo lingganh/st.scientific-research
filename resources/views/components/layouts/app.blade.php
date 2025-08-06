3269bc2c6ddfd59f8a14f3ca76c29b2243184f6b
Hoàng Diệu Linh <dieulinh120411@gmail.com>
2025-08-07 02:09:22 +0700

    fix theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,6 @@
 
          <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
 
@@ -25,8 +24,12 @@
 </div>
 
 @include('components.layouts.footer')
-
-@livewireScripts
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+ <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ @livewireScripts
 
 
 
--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -1,56 +1,96 @@
-<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-<div class="loader-container">
-    <div class="loader"></div>
-</div>
-<div class="overlay"></div>
-<body class="relative">
 <header>
-    <div class="container header-section">
-        <div aria-details="logo" class="logo">
-            <img src="https://vitc.edu.vn/Frond_end/images/logo_vnua-1.png" alt="VNUA Logo" class="vnua-logo">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            {{-- Logo và Brand (Bên trái) --}}
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <div class="logo">
+                    <img src="https://vitc.edu.vn/Frond_end/images/logo_vnua-1.png" alt="VNUA Logo" class="vnua-logo">
+                    <div class="logo-text">
+                        <span>ST Scientific Research</span>
+                        <span class="logo-subtext">Vietnam National University of Agriculture</span>
+                    </div>
+                </div>
+            </a>
 
-            <div class="logo-text">
-                <a href="{{route('home')}}">ST Scientific Research</a>
-                <span class="logo-subtext">Vietnam National University of Agriculture</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {{-- Nội dung menu (sẽ thu gọn) --}}
+            <div class="collapse navbar-collapse" id="mainNavbar">
+                {{-- Các link chính (căn giữa) --}}
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">Trang Chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('product') }}">Sản Phẩm</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('workshop') }}">Hội Thảo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">Bài Viết</a>
+                    </li>
+
+
+                {{-- CÁC LINK CHỮ CHO MOBILE (sẽ ẩn trên desktop) --}}
+                <ul class="navbar-nav d-lg-none">
+                    <li class="nav-item">
+                        <a class="nav-link" href="giohang.html">Giỏ hàng</a>
+                    </li>
+                    @if (Auth::check())
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="mobileUserDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="mobileUserDropdown">
+                                <a class="dropdown-item" href="{{ route('profile.user') }}">Hồ sơ của tôi</a>
+                                <div class="dropdown-divider"></div>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">Đăng xuất</button>
+                                </form>
+                            </div>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Tài khoản</a>
+                        </li>
+                    @endif
+
             </div>
 
-        </div>
-        <div class="menu" id="menu">
-            <span class="line"></span>
-            <span class="line"></span>
-            <span class="line"></span>
-        </div>
-        <nav>
-            <ul class="nav-links" style="margin-left:20%">
-                <li style="--d: 0.1s">
-                    <a href="{{route('home')}}">Trang Chủ </a>
-                </li>
-                <li style="--d: 0.1s">
-                    <a href="{{route('product')}}">Sản Phẩm </a>
-                </li>
-                <li style="--d: 0.2s">
-                    <a href="{{route('workshop')}}">Hội Thảo</a>
-                </li>
-                <li style="--d: 0.3s">
-                    <a href="#about">Bài Viết</a>
-                </li>
-
-
-                <li style="--d: 0.12s">
+            {{-- CÁC ICON CHO DESKTOP (sẽ ẩn trên mobile) --}}
+            <ul class="navbar-nav align-items-center d-none d-lg-flex">
+                <li class="nav-item">
                     <a class="nav-link" href="giohang.html">
-                        <i class="fa fa-shopping-cart cart-icon"></i>  <span class="cart-text">Giỏ hàng</span>       <span class='badge badge-warning' id='lblCartCount'> 5 </span>
+                        <i class="fa fa-shopping-cart cart-icon"></i>
+                        <span class='badge badge-warning' id='lblCartCount'>5</span>
                     </a>
                 </li>
-                    @if (!Auth::check())
+                @if (Auth::check())
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="desktopUserDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa-regular fa-circle-user fa-2x user-icon"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="desktopUserDropdown">
+                            <a class="dropdown-item" href="{{ route('profile.user') }}">{{ Auth::user()->name }}</a>
+                            <div class="dropdown-divider"></div>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="dropdown-item">Đăng xuất</button>
+                            </form>
+                        </div>
+                    </li>
+                @else
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">
-                            <i class="fa-regular fa-circle-user fa-2x user-icon"></i> <span class="user-text">Tài khoản</span>                  </a>
-                    @else
-                        <a class="nav-link user-icon-toggle" href="{{route('profile.user')}}">
-                            <i class="fa-regular fa-circle-user fa-2x user-icon"></i> <span class="user-text">Tài khoản</span>                  </a>
+                            <i class="fa-regular fa-circle-user fa-2x user-icon"></i>
+                        </a>
+                    </li>
                 @endif
-
             </ul>
-        </nav>
-    </div>
+        </div>
+    </nav>
 </header>
